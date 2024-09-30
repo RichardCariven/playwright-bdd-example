dcd6b9de9ea09b2a5777f477b81d9316b505dcd4
Richard Cariven <Richard.Cariven@Bauermedia.co.uk>
2024-09-30 11:26:37 +0100

    removed unneeded files

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,5 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [require.resolve("@rayo/eslint/next")],
+  root: true,
+};
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,7 @@
+// tailwind config is required for editor support
+
+const sharedConfig = require("@rayo/ui/tailwind/config.js");
+
+module.exports = {
+  presets: [sharedConfig],
+};
